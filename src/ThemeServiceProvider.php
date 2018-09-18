eb12a5f7b806ead7cf2a690709a90f10f5ef9eda
Marcio Nobrega <marcio@fibrasites.com.br>
2018-09-18 18:27:19 -0300

    mobi-theme

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Galata\MobiTheme;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::style('mobi-theme', __DIR__.'/../resources/css/theme.css');
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
